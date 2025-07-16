d759b026f5382da4cd7a5a990de995cb572a35f8
Hend Mohammed <hend.odesk.tasks@gmail.com>
2025-07-16 12:40:10 +0300

    add navbar & finish the header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,16 @@
 import Link from '../link'
 import { Routes } from '@/constants/enums'
+import Navbar from './Navbar'
 
 const Header = () => {
     return (
-        <div className='container flex h-16 items-center justify-between '>
-            <Link href={Routes.ROOT}> 🍕 Pizza</Link>
-        </div>
+        // <div className='container flex h-16 items-center justify-between '>
+        <header className='py-4 md:py-6'>
+            <div className='container flex items-center justify-between'>
+                <Link href={Routes.ROOT}> 🍕 Pizza</Link>
+                <Navbar />
+            </div>
+        </header>
     )
 }
 
