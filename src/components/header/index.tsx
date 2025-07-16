28a7b2b4ee8f5e9a29eedfcf5c4b119ac767ae91
Hend Mohammed <hend.odesk.tasks@gmail.com>
2025-07-16 13:02:12 +0300

    fix missing style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,8 +5,8 @@ import Navbar from './Navbar'
 const Header = () => {
     return (
         // <div className='container flex h-16 items-center justify-between '>
-        <header className='py-4 md:py-6'>
-            <div className='container flex items-center justify-between'>
+        <header className='py-4 md:py-6 flex justify-center'>
+            <div className='container flex items-center gap-6 lg:gap-10 justify-between'>
                 <Link href={Routes.ROOT}> 🍕 Pizza</Link>
                 <Navbar />
             </div>
