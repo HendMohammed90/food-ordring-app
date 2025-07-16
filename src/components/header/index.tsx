5e15a259a92cc5fbf38b9fc0a8e9bbc2b3b76a46
Hend Mohammed <hend.odesk.tasks@gmail.com>
2025-07-16 15:08:03 +0300

    create Hero section

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,7 +7,7 @@ const Header = () => {
         // <div className='container flex h-16 items-center justify-between '>
         <header className='py-4 md:py-6 flex justify-center'>
             <div className='container flex items-center gap-6 lg:gap-10 justify-between'>
-                <Link href={Routes.ROOT}> 🍕 Pizza</Link>
+                <Link href={Routes.ROOT} className='text-2xl font-bold'> 🍕 Pizza</Link>
                 <Navbar />
             </div>
         </header>
