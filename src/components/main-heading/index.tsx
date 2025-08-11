cb5432a0bd926eb7767e0120c6b60fde13572a51
Hend Mohammed <hend.odesk.tasks@gmail.com>
2025-08-11 13:06:52 +0300

    fix:styles

--- src/components/main-heading/index.tsx
+++ src/components/main-heading/index.tsx
@@ -1,11 +1,11 @@
 function MainHeading({ title, subTitle }: { title: string; subTitle: string }) {
     return (
-        <>
+        <div className='text-center mb-3'>
             <span className='uppercase text-primary font-semibold leading-4 underline'>
                 {subTitle}
             </span>
             <h2 className='mt-5 text-chart-5 font-bold text-4xl italic'>{title}</h2>
-        </>
+        </div>
     );
 }
 
