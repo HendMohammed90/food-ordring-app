74d8d4e7b7db064530ce6eaa62bc6a215250f101
Hend Mohammed <hend.odesk.tasks@gmail.com>
2025-08-13 12:44:14 +0300

    fix:styles

--- src/server/db/products.ts
+++ src/server/db/products.ts
@@ -1,23 +1,29 @@
 import { cache } from "@/lib/cache";
 import { db } from "@/lib/prisma";
 
-export const getBestSellers =  cache(
+export const getBestSellers = cache(
     () => {
-    return db.product.findMany({
-        take: 3, // Only get first 3 products for the home page
-        orderBy: {
-            orders: {
-                _count: 'desc'
+        return db.product.findMany({
+            take: 3, // Only get first 3 products for the home page
+            orderBy: {
+                orders: {
+                    _count: 'desc'
+                }
+            },
+            include: {
+                sizes: true,
+                extras: true,
+            },
+            where: {
+                orders: {
+                    some: {}
+                }
             }
-        },
-        include: {
-            sizes: true,
-            extras: true,
-        },
-        where: {
-            orders: {
-                some:{}
-            }
-        }
-    });
-}, ['best-sellers'], { revalidate: 60 });
+        });
+    },
+    ['best-sellers'],
+    {
+        revalidate: 60,
+        tags: ['best-sellers']
+    }
+);
